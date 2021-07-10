07c7bdd7b88d6bc533c046565df7cd9d7509f445
AcidDeliria <aleksandra.rzepecka@outlook.com>
2021-07-10 03:43:35 +0200

    created skills file draft

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,5 +1,88 @@
-// import React from "react"
+import React from "react"
 
-// const Skills = () => {
-// }
-// export default Skills;
+const Skills = () => {
+    return(
+        <section className="skills-section">
+            <h2>Skills</h2>
+            <div className="hard-skills-wrapper">
+                <h3>Hard Skills</h3>
+                <div className="skills-wrapper">
+                    <div className="skill-area">
+                        <h4>Web Development</h4>
+                        <div className="department">
+                            <h5>Front-End Development</h5>
+                            <ul>
+                                <li>HTML5/CSS3</li>
+                                <li>SCSS</li>
+                                <li>JavaScript</li>
+                                <li>React</li>
+                                <li>GatsbyJS</li>
+                                <li>Figma(UI/UX design/wireframing/prototyping)</li>
+                            </ul>
+                        </div>
+                        <div className="department">
+                            <h5>Back-End Development</h5>
+                            <ul>
+                                <li>Node.js</li>
+                                <li>Express.js</li>
+                            </ul>
+                        </div>
+                        <div className="department">
+                            <h5>WebGL</h5>
+                            <ul>
+                                <li>A-Frame</li>
+                                <li>Three.js</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="skill-area">
+                        <h4>App Development</h4>
+                        <ul>
+                            <li>React Native</li>
+                        </ul>
+                    </div>
+                    <div className="skill-area">
+                        <h4>Version Control</h4>
+                        <ul>
+                            <li>Git/Github</li>
+                        </ul>
+                    </div>
+                    <div className="skill-area">
+                        <h4>Game Development (learning goals) </h4>
+                        <ul>
+                            <li>C# (basic knowledge)</li>
+                            <li>Unity Engine (basic knowledge)</li>
+                        </ul>
+                    </div>
+                    <div className="skill-area">
+                        <h4>Other</h4>
+                        <ul>
+                            <li>Adobe Photoshop</li>
+                            <li>Adobe Lightroom</li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            <div className="soft-skills-wrapper">
+            <h3>Soft Skills</h3>
+                <div className="skills-wrapper"></div>
+                <ul>
+                    
+                <li>Big-picture thinking</li>
+                <li>Problem solving</li>
+                <li>Pattern recognition</li>
+                <li>Analytical skills</li>
+                <li>Teamwork & Collaboration</li>
+                <li>Empathy/Emotional Intelligence</li>
+                <li>Open Communication</li> 
+                <li>Self-awareness</li>
+                <li>Resourcefullness</li>
+                <li>Adaptability</li>
+                <li>Working remotely</li>
+                </ul>
+                </div>
+        </section>
+    )
+}
+
+export default Skills;
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ const Index = ({ data, location }) => {
             <SEO title="Home" />
             <Hero />
             <About />
+            <Skills />
         </Layout>
     )}
 export default Index
