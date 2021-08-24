97aeb7fbc22eff814ae5e5eeedd2ef077df459f7
AcidDeliria <aleksandra.rzepecka@outlook.com>
2021-08-24 18:55:37 +0200

    adjusted fading in, buttons disappearing, timing on about

--- src/components/about.js
+++ src/components/about.js
@@ -8,25 +8,28 @@ const About = () => {
     const [isOpenText2, setIsOpenText2] = useState(false)
     const [textFadeIn, setTextFadeIn] = useState(false)
     const [deleteButton, setDeleteButton] = useState(false)
+    const [delete2ndButton, setDelete2ndButton] = useState(false)
+    const [showSecondButton, setShowSecondButton ] = useState(false)
 
   const handleClick = () => {
     console.log("Button clicked")
     setIsOpenText1(true)
     setTimeout(() => {setDeleteButton(true)}, 200)
-    // setTimeout(() => {setIsOpenText2(true)}, 5000)
-   // setTimeout(() => {setTextFadeIn(true)}, 4000)
+    setTimeout(() => {setShowSecondButton(true)}, 6000)
   }
+
   const handleClick2 = () => {
-      //TODO 
     console.log("Another Button clicked")
     setIsOpenText2(true)
+    setTimeout(() => {setDelete2ndButton(true)}, 200)
     setTextFadeIn(true)
   }
 const buttonProps = { 
 className: deleteButton ? "delete" : ""
 }
-//TODO: Button2 props
-
+const buttonProps2 = {
+    className: delete2ndButton ? "delete" : ""
+}
 
 return (
     <section id="about">
@@ -45,11 +48,11 @@ return (
                 <p><em>But</em>, first thing that you should know about me - <strong><em>I am a big-picture thinker</em></strong>.<br/>
                 It all starts with the mindset. I want to give you an insight into <em>how</em> I think, <em>what</em> actions I take, and <em>why</em> I do it this way.</p>
 
-                <p>Here’s a glimpse of my background that shaped me, so that it can help you get a sense of what kind of person I am. Each experience has taught me something valuable that helped me in other settings, but in a different form.</p>
+                <p>Here’s a glimpse of my background that shaped me, so that it can help you get a sense of what kind of person I am. Each of these 5 points has taught me something valuable that helped me in other settings, but in a different form.</p>
                 <button {...buttonProps} onClick={handleClick}>Get to know me</button>
                 <Expand open={isOpenText1}>
                         <ul className="about-me-facts">
-                        <FadeIn className="facts" delay={1200} transitionDuration={800} >
+                        <FadeIn className="facts" delay={1000} transitionDuration={500} >
                             <li>As a teenager I became licenced glider pilot.</li>
                             <li>I came to Sweden 5 years ago, on my own, with a plan and money... that I haven’t had.</li>
                             <li>I worked in retail. I've also worked various fast-pace service jobs in the restaurant industry. And my latest job position I’ve had (before starting my frontend education) was a dog walker/caretaker.</li>
@@ -57,14 +60,13 @@ return (
                             <li>I also collaborated with a few internationally recognized alternative clothing brands when I was growing my social media reach. Along with that, I picked up interest in photography and photo editing using Adobe programs.</li>
                         </FadeIn>
                         </ul>
-                        { // TODO: FADE IN ON BUTTON AND TEXT }
-                        <FadeIn delay={1400} transitionDuration={400}>
+                        <FadeIn className="about-me-question" visible={showSecondButton}  delay={1000} transitionDuration={400}>
                             <p>...where am I going with that?</p>
-                            <button onClick={handleClick2}>Get to understand me</button>
+                            <button {...buttonProps2} onClick={handleClick2}>Get to understand me</button>
                         </FadeIn>
                </Expand>
                <Expand open={isOpenText2}>
-                <FadeIn className="about-me-text-2" visible={textFadeIn} delay={2000} transitionDuration={800}>
+                <FadeIn className="about-me-text-2" visible={textFadeIn} delay={1000} transitionDuration={700}>
                     <p>Experimenting with creative softwares has led me to becoming <del>obsessed</del> <ins>passionate</ins> about coding and development, which allows me to create new value utilising already gained skills/perspectives/experiences as contributions to new solutions</p>
 
                     <p><em> Taking chances</em> and <em>calculating risks</em>, required <strong><em>resourcefullness</em></strong>, <strong><em>flexibility</em></strong> and <strong><em>adaptation</em></strong> to new situations.</p>
