c3a196a65fad680c164d4264096e293ed9d10b3b
AcidDeliria <aleksandra.rzepecka@outlook.com>
2021-08-24 02:21:19 +0200

    added second button to follow up on about(draft) , wrote todos to continue on next day

--- src/components/about.js
+++ src/components/about.js
@@ -13,12 +13,19 @@ const About = () => {
     console.log("Button clicked")
     setIsOpenText1(true)
     setTimeout(() => {setDeleteButton(true)}, 200)
-    setTimeout(() => {setIsOpenText2(true)}, 5000)
-    setTimeout(() => {setTextFadeIn(true)}, 4000)
+    // setTimeout(() => {setIsOpenText2(true)}, 5000)
+   // setTimeout(() => {setTextFadeIn(true)}, 4000)
+  }
+  const handleClick2 = () => {
+      //TODO 
+    console.log("Another Button clicked")
+    setIsOpenText2(true)
+    setTextFadeIn(true)
   }
 const buttonProps = { 
 className: deleteButton ? "delete" : ""
 }
+//TODO: Button2 props
 
 
 return (
@@ -38,11 +45,11 @@ return (
                 <p><em>But</em>, first thing that you should know about me - <strong><em>I am a big-picture thinker</em></strong>.<br/>
                 It all starts with the mindset. I want to give you an insight into <em>how</em> I think, <em>what</em> actions I take, and <em>why</em> I do it this way.</p>
 
-                <p>Here’s a glimpse of my background that shaped me, so that it can help you get a sense of what kind of person I am:</p>
-                <button {...buttonProps} onClick={handleClick}>Click here</button>
+                <p>Here’s a glimpse of my background that shaped me, so that it can help you get a sense of what kind of person I am. Each experience has taught me something valuable that helped me in other settings, but in a different form.</p>
+                <button {...buttonProps} onClick={handleClick}>Get to know me</button>
                 <Expand open={isOpenText1}>
                         <ul className="about-me-facts">
-                        <FadeIn className="facts" delay={1200} transitionDuration={800}>
+                        <FadeIn className="facts" delay={1200} transitionDuration={800} >
                             <li>As a teenager I became licenced glider pilot.</li>
                             <li>I came to Sweden 5 years ago, on my own, with a plan and money... that I haven’t had.</li>
                             <li>I worked in retail. I've also worked various fast-pace service jobs in the restaurant industry. And my latest job position I’ve had (before starting my frontend education) was a dog walker/caretaker.</li>
@@ -50,21 +57,24 @@ return (
                             <li>I also collaborated with a few internationally recognized alternative clothing brands when I was growing my social media reach. Along with that, I picked up interest in photography and photo editing using Adobe programs.</li>
                         </FadeIn>
                         </ul>
+                        { // TODO: FADE IN ON BUTTON AND TEXT }
+                        <FadeIn delay={1400} transitionDuration={400}>
+                            <p>...where am I going with that?</p>
+                            <button onClick={handleClick2}>Get to understand me</button>
+                        </FadeIn>
                </Expand>
                <Expand open={isOpenText2}>
-                <FadeIn className="about-me-text-2" visible={textFadeIn} delay={3000} transitionDuration={1000}>
-                    <p>Upon getting an insight into my experiences, it should be easier to tie other traits into the picture.<br/>
-                    Each experience has taught me something valuable that helped me in other settings, but in a different form.</p>
-                    <p>Experimenting with creative softwares has led me to becoming <del>obsessed</del> <ins>passionate</ins> about coding and development, which allows me to create new value utilising already gained skills/perspectives/experiences as contributions to new solutions
+                <FadeIn className="about-me-text-2" visible={textFadeIn} delay={2000} transitionDuration={800}>
+                    <p>Experimenting with creative softwares has led me to becoming <del>obsessed</del> <ins>passionate</ins> about coding and development, which allows me to create new value utilising already gained skills/perspectives/experiences as contributions to new solutions</p>
 
-                    <em> Taking chances</em> and <em>calculating risks</em>, required <strong><em>resourcefullness</em></strong>, <strong><em>flexibility</em></strong> and <strong><em>adaptation</em></strong> to new situations. 
-                    Deep diving head-first into new settings was always conscious choice of forcing my development and challenging myself in unknown areas.</p>
+                    <p><em> Taking chances</em> and <em>calculating risks</em>, required <strong><em>resourcefullness</em></strong>, <strong><em>flexibility</em></strong> and <strong><em>adaptation</em></strong> to new situations.</p>
+                    <p>Deep diving head-first into new settings was always conscious choice of forcing my development and challenging myself in unknown areas.</p>
                 
-                    <p> My extroverted nature of a curious kid plays probably the biggest role; through socialising, empathy, and A LOT OF QUESTIONS <em>(yes, I am THAT person)</em> I got to learn new things and understand different perspectives - <em>see the bigger picture</em>, analyse it, and look for learning opportunities. 
+                    <p> My extroverted nature of a curious kid plays probably the biggest role; through socialising, empathy, and A LOT OF QUESTIONS <em>(yes, I am THAT person)</em> I got to learn new things and understand different perspectives - <em>see the bigger picture</em>, analyse it, and look for learning opportunities. <br/>
+                    If there's something I can learn from YOU - 
                     </p>
                  </FadeIn>
                  </Expand>
-                
                 <Button text="Let's Talk" />
         </div>
     
