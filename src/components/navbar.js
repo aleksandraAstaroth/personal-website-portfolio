cc142af5ac6d8b260b7928747aca9e3d329701cf
AcidDeliria <aleksandra.rzepecka@outlook.com>
2021-09-02 17:35:37 +0200

    started mobile styling, hero

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,9 @@ export default function Navbar() {
         <nav>
             <ul>
                 <li><AnchorLink className="nav-link" to="/">Home</AnchorLink></li>
-                <li><AnchorLink className="nav-link" to="#about">Get to know me</AnchorLink></li>
-                <li><AnchorLink  className="nav-link" to="#skills">Skills</AnchorLink></li>
+                <li><AnchorLink className="nav-link" to="#about">About</AnchorLink></li>
                 <li><AnchorLink  className="nav-link" to="#portfolio">Portfolio</AnchorLink></li>
+                <li><AnchorLink  className="nav-link" to="#skills">Skills</AnchorLink></li>
                 <li><AnchorLink  className="nav-link" to="#portfolio">Contact</AnchorLink ></li>
             </ul>
         </nav>
