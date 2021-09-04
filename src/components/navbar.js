5f75c8173014decaa34b77fba5e6b71687324ef5
AcidDeliria <aleksandra.rzepecka@outlook.com>
2021-09-04 19:34:40 +0200

    added animation on hamburger

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,17 +13,20 @@ export default function Navbar() {
         console.log("button clicked")
         setShowHamburger(!showHamburger)
         setStylingMobile(!stylingMobile)
-        
-        
+        animateHamburgerButton()
     }
+    function animateHamburgerButton() {
+        let lines = Array.from(document.querySelectorAll(".hamburger span"))
+        lines.forEach(line => line.style.animation = `${line.className} 0.3s ease ${showHamburger ? "reverse" : "normal"}`);
+      }
 
     return (
         <nav {...navListProps}>
         <div className="hamburger-wrapper">  
             <button className="hamburger" aria-label="hamburger-menu" onClick={toggleHamburgerMenu}>
-                <span></span>
-                <span></span>
-                <span></span>
+                <span className="line1" style={{ transform: showHamburger ? "translateY(9px) rotate(45deg)" : "translateY(0px) rotate(0deg)" }} onAnimationEnd={(e) => e.target.style.animation = "none"}></span>
+                <span className="line2" style={{ opacity: showHamburger ? 0 : 1 }} onAnimationEnd={(e) => e.target.style.animation = "none"}></span>
+                <span className="line3"style={{ transform: showHamburger ? "translateY(-9px) rotate(-45deg)" : "translateY(0px) rotate(0deg)" }}onAnimationEnd={(e) => e.target.style.animation = "none"}></span>
             </button>
         </div>
             <ul className={ showHamburger ? "" : "hide"}>
