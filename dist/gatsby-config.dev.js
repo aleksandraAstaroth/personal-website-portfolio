9b9b9005153997303a7a5361fdef32f7560047c7
AcidDeliria <aleksandra.rzepecka@outlook.com>
2021-09-07 14:52:59 +0200

    updated icon

--- dist/gatsby-config.dev.js
+++ dist/gatsby-config.dev.js
@@ -83,7 +83,7 @@ module.exports = {
       background_color: "#ffffff",
       theme_color: "#663399",
       display: "minimal-ui",
-      icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
+      icon: "src/images/website-icon.png" // This path is relative to the root of the site.
 
     }
   }, {
