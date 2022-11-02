99497fc52f02fe5d7edf3a654376a49a82f64a03
AcidDeliria <aleksandra.rzepecka@outlook.com>
2022-11-02 18:53:12 +0100

    moved projectcard into scss module

--- src/components/projectcard.js
+++ src/components/projectcard.js
@@ -1,17 +1,18 @@
 import React from "react"
 import ProjectImage from "../components/project-image"
 import Tags from "../components/tags"
+import * as style from "../styles/projectcard/projectcard.module.scss"
 
 const Card = (props) => {
     return(
-        <div className="project-container">
-                <div className="project-inner-container">
-                        <div className="project-img-wrapper">
-                            <ProjectImage className="project-img" src={props.projectPreview} alt="project preview image" />
+        <div className={style.projectContainer}>
+                <div className={style.projectInnerContainer}>
+                        <div className={style.projectImgWrapper}>
+                            <ProjectImage className={style.projectImg} src={props.projectPreview} alt="project preview image" />
                         </div>
-                        <div className="gradient"></div>
-                        <div className="project-info">
-                                <Tags className="tag-container"tagList={props.tags}/>
+                        <div className={style.gradient}></div>
+                        <div className={style.projectInfo}>
+                                <Tags className={style.tagContainer} tagList={props.tags}/>
                                 <h5>{props.projectName}</h5>
                                 <hr></hr>
                                 <p>{props.projectInfo}</p>
